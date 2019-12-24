e9e20c20bda8435be89282f2b3fe4cf32e4ac82a
Dave Kerr <dwmkerr@gmail.com>
2019-12-24 23:36:05 +0545

    refactor: move all of the posts into folders

--- scripts/restructure.sh
+++ scripts/restructure.sh
@@ -1,9 +1,23 @@
 #!/usr/bin/env bash
 
+# Go through each post.
 for post_path in dwmkerr.com/content/post/*.md; do
     echo "Found $post_path"
     filename=$(basename -- "$post_path")
-    extension="${filename##*.}"
     filename="${filename%.*}"
-    echo "   $filename     - $extension"
+
+    # Grep out the date line.
+    dateline=$(grep -E "^date: " "$post_path")
+
+    # We know how to get the year as the date line is consistent in all posts:
+    # date: "2012-12-09T16:11:27Z"
+    year=${dateline:7:4}
+
+    # Create the folder for the post.
+    new_folder="dwmkerr.com/content/post/$year/$filename"
+    mkdir -p "$new_folder"
+
+    # Move the post.
+    mv "$post_path" "$new_folder/index.md"
+    echo "  -> $new_folder/index.md"
 done
