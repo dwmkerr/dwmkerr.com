e2d4792aeaf44b0532d85e151ccfeb3e8b4c6e73
Dave Kerr <dwmkerr@gmail.com>
2020-01-02 22:31:20 +0545

    chore: remove stale code from helper script

--- scripts/collect-images.js
+++ scripts/collect-images.js
@@ -199,52 +199,6 @@ function processPost(rootPath, postPath) {
   });
 }
 
-// #     # Find all HTML image tags.
-// #     image_paths=$(grep -Eoi '<img [^>]+>' $post_path |
-// #         grep -Eo 'src="[^\"]+"' |
-// #         sed -e 's/src=\"\(.*\)\"/\1/g')
-
-// #     # If we found no images, move on.
-// #     [ -n "$image_paths" ] || continue
-
-// #     # For every image found, prepare to copy to the post directory.
-// #     for image_path in $image_paths; do
-// #         image_name="$(basename $image_path)"
-// #         destination_folder="$(dirname $post_path)/images"
-// #         destination="${destination_folder}/${image_name}"
-
-// #         # Create the destination folder.
-// #         mkdir -p "$destination_folder"
-
-// #         # if the image starts with HTTP, we'll download it.
-// #         if [[ $image_path =~ ^http ]]; then
-// #             set -o xtrace
-// #             # wget "$image_path" -O "$destination"
-// #             set +o xtrace
-// #         elif [[ $image_path =~ ^/wp-content ]]; then
-// #             set -o xtrace
-// #             cp "dwmkerr.com/static/$image_path" "${destination_folder}"
-// #             set +o xtrace
-// #         else
-// #             set -o xtrace
-// #             # cp "dwmkerr.com/static/$image_path" "${destination_folder}"
-// #             set +o xtrace
-// #         fi
-// #     done
-
-// #     # Note this fails for the git downloaded images.
-// #     # Note this fixes the wp-upload folders, finally simplifynig the structure.
-// #     # Note that we can strip out some stuff with this:
-// #     # :%s/https:\/\/github.com\/dwmkerr\/effective-shell\/blob\/master\/1-navigating-the-command-line\///gc 
-// #     # Note that embedded html such as tables is omitted.
-
-// #     # perl -p -e 's/\<img\s+width="(\d+px)"\s+alt="([^\"]+)"\s+src="([^\"]+)"\s\/\>/{{< figure src="$3" title="$2" width="$1" >}}/g' "$post_path" 
-
-// #     # Grep out any markdown images.
-
-// # done
-// }
-
 console.log("collect-images: Tool to co-locate blog post images")
 console.log("");
 
